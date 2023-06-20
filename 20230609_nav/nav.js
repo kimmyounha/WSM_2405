7fee127ffb5023c36d892348a79ca14bc0f609cb
kimmyounha <w2224@e-mirim.hs.kr>
2023-06-20 11:06:41 +0900

    nav

--- 20230609_nav/nav.js
+++ 20230609_nav/nav.js
@@ -1,7 +1,6 @@
 function sayHello(){
     alert("안녕하시와요");
 }
-sayHello();
 
 //1. html -> js
 //햄버거 메뉴 -> navToggleDiv
@@ -9,14 +8,28 @@ sayHello();
 //.nav-list -> navListUl
 //const navToggleDiv = document.getElementById("nav_toggle");
 
-const nevToggleI = nav_toggle.getElementsByTagName("i")[0];
-const navListUl = document.getElementsByClassName("nav-list")[0];
+const navToggleI = nav_toggle.getElementsByTagName("i")[0];         
+const navListUl = document.getElementsByClassName("nav-list")[0]; 
 
 //2. Event Hendling
-//햄버거 메뉴 클릭하면, 햄버거메뉴 i "bi bi-list"를 x "bi bi-x-lg"로 
-//햄버거 메뉴 클랙하면, .nav-list에 .show-menu 보여주기 <-> 안보여주기
-nav_toggle.onclick = () =>alert("김윤하이올시다");//ok
-//nav_toggle.onclick = alert("안녕"); //not ok
+//nav_toggle.onclick = () =>alert("안녕");  //ok
+//nav_toggle.onclick = alert("안녕");       //not ok
+// nav_toggle.onclick = () => alert("안녕");  // 시험 나옴 // 바로 실행되지 않음 // 잘 ok
+// nav_toggle.onclick = alert("안녕");     // 바로 실행됨  // not ok
+
+nav_toggle.onclick = () => {
+    // 햄버거 메뉴를 클릭하면, 햄버거메뉴 i <-> <i class="bi bi-x-lg"></i>
+    navToggleI.classList.toggle("bi-list");
+    navToggleI.classList.toggle("bi-x-lg");
+
+    // 햄버거 메뉴를 클릭하면, .nav=list에 .show-menu 보여주기 <-> 안보여주기
+    navListUl.classList.toggle("show-menu");
+}
+
+
+
+
+
 //-----------------------------------
 //HTML -> js
 //id
